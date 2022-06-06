2d93b15cc67efe6f3eeaa5567eb54104067527b0
AbdallahAlyamni <AbdallahAlyamni@gmail.com>
2022-06-06 13:07:47 +0200

    Update index.js
    change footer

--- pages/index.js
+++ pages/index.js
@@ -87,8 +87,8 @@ export default function Home() {
       </div>
       <h1 className="text-sm text-bold mt-10 mob:mt-2 laptop:mt-10 mob:p-2 laptop:p-0">
         Made With ❤ by{" "}
-        <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
+        <Link href="https://www.linkedin.com/in/abdallahalyamni/">
+          <a className="underline underline-offset-1">Abdallah Alyamni</a>
         </Link>
       </h1>
     </div>
